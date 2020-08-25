a0c5b8e200fc27dc1883c5c4f8668c8a29911327
shawnjonchan <xiaojiechen86@gmail.com>
2020-08-25 15:13:54 +0800

    创建<Layout />

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">Labels.vue</div>
-    <Nav />
-  </div>
+  <Layout>Labels.vue</Layout>
 </template>
 
 <script lang='ts'>
@@ -10,17 +7,3 @@ export default {
   name: "Labels",
 };
 </script>
-
-<style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid green;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content {
-  border: 1px solid blue;
-  overflow: auto;
-  flex-grow: 1;
-}
-</style>
