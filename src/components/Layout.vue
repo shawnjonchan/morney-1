f51a2737ff5223f549b6606f8ef0bc09cf7f620d
shawnjonchan <xiaojiechen86@gmail.com>
2020-08-26 10:47:48 +0800

    添加active-class和手机meta

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -7,7 +7,7 @@
   </div>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 export default {
   name: "Layout",
 };
@@ -15,13 +15,11 @@ export default {
 
 <style lang="scss" scoped>
 .nav-wrapper {
-  border: 1px solid green;
   display: flex;
   flex-direction: column;
   height: 100vh;
 }
 .content {
-  border: 1px solid blue;
   overflow: auto;
   flex-grow: 1;
 }
