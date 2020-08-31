4a4088732482956a188a70ba7c31e56d7f9c46e5
shawnjonchan <xiaojiechen86@gmail.com>
2020-08-31 16:09:04 +0800

    封装出一个 model

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,26 +14,17 @@ import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component, Watch } from "vue-property-decorator";
+import model from "@/model.ts";
 
-const recordList: Record[] = JSON.parse(
-  window.localStorage.getItem("recordLiset") || "[]"
-);
-
-type Record = {
-  tags: string[];
-  notes: string;
-  type: string;
-  amount: number; // 数据类型 object等
-  createdAt?: Date; // 类 /构造函数
-};
+const recordList = model.fetch();
 
 @Component({
   components: { Tags, Notes, Types, NumberPad },
 })
 export default class Money extends Vue {
   tags = ["衣", "食", "住", "行", "旅游"];
-  recordList: Record[] = recordList;
-  record: Record = {
+  recordList: RecordItem[] = recordList;
+  record: RecordItem = {
     tags: [],
     notes: "",
     type: "-",
@@ -47,13 +38,13 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    const record2: RecordItem = model.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
   }
   @Watch("recordList")
   onRecordListChange() {
-    window.localStorage.setItem("recordList", JSON.stringify(this.recordList));
+    model.save(this.recordList);
   }
 }
 </script>
