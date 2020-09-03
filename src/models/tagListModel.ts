c5480d89745011721b118daa10fa115e7c211e29
shawnjonchan <xiaojiechen86@gmail.com>
2020-09-03 10:58:02 +0800

    使用全局 tagList

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,18 +1,6 @@
 import createId from "@/lib/createId";
 
 const localStorageKeyName = "tagList";
-type Tag = {
-  id: string;
-  name: string;
-};
-type TagListModel = {
-  data: Tag[];
-  fetch: () => Tag[];
-  create: (name: string) => "success" | "duplicated"; // 联合类型
-  update: (id: string, name: string) => "success" | "not found" | "duplicated";
-  remove: (id: string) => boolean;
-  save: () => void;
-};
 const tagListModel: TagListModel = {
   data: [],
   fetch() {
@@ -27,8 +15,8 @@ const tagListModel: TagListModel = {
     if (names.indexOf(name) >= 0) {
       return "duplicated";
     }
-    const id =createId().toString();
-    this.data.push({id,name:name});
+    const id = createId().toString();
+    this.data.push({ id, name: name });
     this.save();
     return "success";
   },
