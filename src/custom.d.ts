13e2c66b29764f437caa9467e0ea25dd7fc836f0
shawnjonchan <xiaojiechen86@gmail.com>
2020-09-04 11:13:23 +0800

    封装 record 的操作

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -28,4 +28,6 @@ interface Window {
     name: string
   ) => "success" | "not found" | "duplicated";
   findTag: (id: string) => Tag | undefined;
+  recordList: RecordItem[];
+  createRecord: (record: RecordItem) => void;
 }
