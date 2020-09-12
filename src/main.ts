99733a15d03a0424ccb1920b303f308a9e4241ea
shawnjonchan <xiaojiechen86@gmail.com>
2020-09-12 15:04:13 +0800

    修复一进页面就弹出四个提示的 bug

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ new Vue({
 
 window.onload = function() {
   setTimeout(function() {
-    window.scrollTo(0, 100);
+    window.scrollTo(0, 200);
   }, 0);
 };
--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 
+const map: { [key: string]: string } = {
+  "tag name duplicated": "标签名重复了",
+};
+
 @Component
 export class TagHelper extends Vue {
   createTag() {
@@ -9,6 +13,13 @@ export class TagHelper extends Vue {
       return window.alert("标签名不能为空");
     }
     this.$store.commit("createTag", name);
+    if (this.$store.state.createTagError) {
+      if (this.$store.state.createTagError.message === "tag name duplicated") {
+        window.alert(
+          map[this.$store.state.createTagError.message] || "未知错误"
+        );
+      }
+    }
   }
 }
 
